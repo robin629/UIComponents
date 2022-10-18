93412a5bb63ede94f1b58910267a376d47af99f3
Robin Israel <robin629@me.com>
2022-10-18 03:30:39 -0700

    Added NOT boolean operator for Binding<Bool> types. This allows things like !$exampleVariable

---
+++ Sources/Extensions/BindingBool.swift
@@ -0,0 +1,15 @@
+//
+//  BindingBool.swift
+//  
+//
+//  Created by Robin Israel on 10/18/22.
+//
+
+import SwiftUI
+
+extension Binding where Value == Bool {
+	
+	static prefix func ! (_ rhs: Binding<Bool>) -> Binding<Bool> {
+		return Binding<Bool>(get: { !rhs.wrappedValue }, set: { rhs.wrappedValue = !$0 })
+	}
+}
