28276190245b3fc7b6ec7ab21c7670adfe0f7a97
Robin Israel <robin629@me.com>
2022-12-15 15:43:11 -0800

    Added new initializer methods to SidebarItem to allow init without using closures.

--- Sources/Sidebar/SidebarItem.swift
+++ Sources/Sidebar/SidebarItem.swift
@@ -8,35 +8,76 @@
 import SwiftUI
 
 public struct SidebarItem<V, Label, Destination>: View where V: Hashable, Label: View, Destination: View {
-	let destination: () -> Destination
-	let label: 	 	 () -> Label
-	var tag: 		 V? = nil
+	let destination: Destination
+	let label: 	 	 Label
+	var tag: 		 V?
 	var selection: 	 Binding<V?>? = nil
 	
-	public init(@ViewBuilder destination: @escaping () -> Destination,
-		 @ViewBuilder label: 	   @escaping () -> Label) {
+	public init(tag: V, selection: Binding<V?>, destination: Destination, label: Label) {
 		self.destination = destination
 		self.label 	 	 = label
+		self.selection	 = selection
+		self.tag		 = tag
 	}
 	
-	public init(tag: V,
-		 selection: Binding<V?>,
-		 @ViewBuilder destination: @escaping () -> Destination,
-		 @ViewBuilder label: 	   @escaping () -> Label) {
-		self.destination = destination
+	public init(tag: V, selection: Binding<V?>, label: Label, @ViewBuilder destination: @escaping () -> Destination) {
+		self.destination = destination()
 		self.label 	 	 = label
-		
-		self.tag = tag
-		self.selection = selection
+		self.selection	 = selection
+		self.tag		 = tag
+	}
+	
+	public init(tag: V, selection: Binding<V?>, destination: Destination, @ViewBuilder label: @escaping () -> Label) {
+		self.destination = destination
+		self.label 	 	 = label()
+		self.selection	 = selection
+		self.tag		 = tag
+	}
+	
+	public init(tag: V, selection: Binding<V?>, @ViewBuilder destination: @escaping () -> Destination, @ViewBuilder label: @escaping () -> Label) {
+		self.destination = destination()
+		self.label 	 	 = label()
+		self.selection	 = selection
+		self.tag 		 = tag
 	}
 
 	public var body: some View {
 		if let tag = self.tag, let selection = self.selection {
-			NavigationLink(tag: tag, selection: selection, destination: destination, label: label)
+			NavigationLink(tag: tag, selection: selection) {
+				destination
+			} label: {
+				label
+			}
 		} else {
-			NavigationLink(destination: destination, label: label)
+			NavigationLink {
+				destination
+			} label: {
+				label
+			}
 		}
     }
+}
+
+extension SidebarItem where V == Never {
+	public init(destination: Destination, label: Label) {
+		self.destination = destination
+		self.label		 = label
+	}
+	
+	public init(label: Label, @ViewBuilder destination: @escaping () -> Destination) {
+		self.destination = destination()
+		self.label		 = label
+	}
+	
+	public init(destination: Destination, @ViewBuilder label: @escaping () -> Label) {
+		self.destination = destination
+		self.label		 = label()
+	}
+	
+	public init(@ViewBuilder destination: @escaping () -> Destination, @ViewBuilder label: @escaping () -> Label) {
+		self.destination = destination()
+		self.label		 = label()
+	}
 }
 
 struct SidebarItem_Previews: PreviewProvider {
@@ -52,25 +93,39 @@ struct SidebarItem_Previews: PreviewProvider {
 		var body: some View {
 			NavigationView {
 				Sidebar {
+					SidebarItem(tag: Views.View1, selection: $selection, destination: Text("Detail View 0"), label: Text("Detail 0"))
+
+					SidebarItem(tag: Views.View1, selection: $selection, destination: Text("Detail View 1")) {
+						Text("Detail 1")
+					}
+					
+					SidebarItem(tag: Views.View2, selection: $selection, label: Text("Detail 2")) {
+						Text("Detail View 2")
+					}
+
 					SidebarItem(tag: Views.View0, selection: $selection) {
-						Text("View 0")
+						Text("Detail View 3")
 					} label: {
-						Text("Row 0")
+						Text("Detail 3")
 					}
 					
-					SidebarItem(tag: Views.View1, selection: $selection) {
-						Text("View 1")
-					} label: {
-						Text("Row 1")
+					SidebarItem(destination: Text("Detail View 4"), label: Text("Detail 4"))
+					
+					SidebarItem(destination: Text("Detail View 5")) {
+						Text("Detail 5")
 					}
 					
-					SidebarItem(tag: Views.View2, selection: $selection) {
-						Text("View 2")
-					} label: {
-						Text("Row 2")
+					SidebarItem(label: Text("Detail 6")) {
+						Text("Detail View 6")
 					}
+					
+					SidebarItem {
+						Text("Detail View 7")
+					} label: {
+						Text("Detail 7")
+					}
+
 				}
-				.navigationTitle("Sidebar Section")
 			}
 		}
 	}
