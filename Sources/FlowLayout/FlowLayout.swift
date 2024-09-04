1d3eb5361bf589e1d263079f4727d179b67986e3
Robin Israel <risrael@company32.com>
2024-09-04 16:02:22 -0700

    Added FlowLayout view.

---
+++ Sources/FlowLayout/FlowLayout.swift
@@ -0,0 +1,68 @@
+//
+//  FlowLayout.swift
+//
+//
+//  Created by Robin Israel on 9/4/24.
+//
+
+import SwiftUI
+
+@available(iOS 16.0, macOS 13.0, *)
+public struct FlowLayout: Layout {
+	var alignment: Alignment = .center
+	var spacing: CGFloat = 10
+	
+	func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+		let maxWidth = proposal.replacingUnspecifiedDimensions().width
+		let sizes 	 = subviews.map { $0.sizeThatFits(.unspecified) }
+		
+		return generateLayout(maxWidth: maxWidth, subviewSizes: sizes).containerSize
+	}
+	
+	func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+		let sizes 	= subviews.map { $0.sizeThatFits(.unspecified) }
+		let offsets = generateLayout(maxWidth: bounds.width, subviewSizes: sizes).itemOffsets
+		
+		for (offset, subview) in zip(offsets, subviews) {
+			subview.place(
+				at: CGPoint(x: bounds.minX + offset.x, y: bounds.minY + offset.y),
+				proposal: .unspecified
+			)
+		}
+	}
+	
+	private func generateLayout(maxWidth: CGFloat, subviewSizes: [CGSize]) -> (itemOffsets: [CGPoint], containerSize: CGSize) {
+		var offsets  = [CGPoint]()
+		var position = CGPoint()
+		var height   = CGFloat(0)
+		var maxX	 = CGFloat(0)
+		
+		for size in subviewSizes {
+			
+			// Reset the X coordinate if needed
+			if (position.x + size.width > maxWidth) {
+				position.x = 0
+				position.y += height + spacing
+				
+				height = 0
+			}
+			
+			// Add item to offsets
+			offsets.append(position)
+			
+			// Update the view's X offset
+			position.x += size.width
+			
+			// Find the new max X position after adding item to view
+			maxX = max(maxX, position.x)
+			
+			// Update the next item's starting X by adding the padding
+			position.x += spacing
+			
+			// Update the total flow layout height
+			height = max(height, size.height)
+		}
+		
+		return (offsets, CGSize(width: maxX, height: position.y + height))
+	}
+}
